9c58af2bdb36a767c334f9faaadab759cf20e541
Toeler <dkmenice@gmail.com>
2017-11-19 16:04:10 +1300

    Fix showSelectedTransaction jumping to wrong row
    Change showSelectedTransaction to calculate the height of the rows above the selected trasnaction for a more accurate jump position

--- sauce/features/budget/activity-transaction-link/index.js
+++ sauce/features/budget/activity-transaction-link/index.js
@@ -62,39 +62,19 @@ export class ActivityTransactionLink extends Feature {
   }
 
   showSelectedTransaction = (accountsController, contentResults, transactionIndex) => {
-    var ynabGrid = ynabToolKit.shared.getEmberView($('.ynab-grid').attr('id'));
-    var ynabGridContainer = ynabToolKit.shared.getEmberView($('.ynab-grid-container').attr('id'));
-    var recordHeight = ynabGridContainer.get('recordHeight');
+    const ynabGrid = ynabToolKit.shared.getEmberView($('.ynab-grid').attr('id'));
+    const ynabGridContainer = ynabToolKit.shared.getEmberView($('.ynab-grid-container').attr('id'));
+    const $ynabGridElement = $(ynabGridContainer.element);
+    const rowsAboveTransaction = $ynabGridElement.find('.ynab-grid-body > .ynab-grid-body-row').slice(0, transactionIndex);
 
     Ember.run.later(() => {
-      var skipSplits = ynabToolKit.options.toggleSplits && ynabToolKit.toggleSplits.setting === 'hide';
-      var transactionScrollTo = recordHeight * transactionIndex;
+      const transactionScrollTo = Array.from(rowsAboveTransaction).reduce((sum, row) => {
+        sum += $(row).outerHeight();
+        return sum;
+      }, 0);
 
-      $(ynabGridContainer.element).scrollTop(transactionScrollTo);
-
-      // if the toggle splits feature is enabled and we're hiding splits, then we need to recalculate
-      // the actual scroll position of the transaction we're linking to.
-      if (skipSplits) {
-        Ember.run.later(() => {
-          var newIndex = transactionIndex;
-          for (var i = ynabGridContainer.get('displayStart'); i < transactionIndex; i++) {
-            if (contentResults[i].get('parentEntityId')) {
-              newIndex--;
-            }
-          }
-
-          // there is a weird interaction with this feature and the toggle splits (i think) that causes
-          // the transaction to still be hidden (just out of view at viewable transaction - 1). Unfortunately
-          // getting that transaction isn't as simple as scrolling one more transaction so we're going
-          // to scroll 5 more transactions. this isn't perfect, it's as magic number as it gets but it works until
-          // i can pinpoint the actual issue here.
-          var newTransactionScrollTo = recordHeight * (newIndex - 5);
-          ynabGrid.uncheckAllBut(this.selectedTransaction);
-          $(ynabGridContainer.element).scrollTop(newTransactionScrollTo);
-        }, 250);
-      } else {
-        ynabGrid.uncheckAllBut(this.selectedTransaction);
-      }
+      $ynabGridElement.scrollTop(transactionScrollTo);
+      ynabGrid.uncheckAllBut(this.selectedTransaction);
     }, 250);
   }
 
